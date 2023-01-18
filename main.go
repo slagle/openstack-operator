5b7871a7c80a98738807afe1aa932982ac59d17a
James Slagle <jslagle@redhat.com>
2023-01-18 08:51:51 -0500

    Initial generation of OpenStackDataPlane
    Signed-off-by: James Slagle <jslagle@redhat.com>

--- main.go
+++ main.go
@@ -146,6 +146,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "TransportURL")
 		os.Exit(1)
 	}
+	if err = (&corecontrollers.OpenStackDataPlaneReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "OpenStackDataPlane")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
